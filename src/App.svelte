eb07370bbb1406601a80e776e5f0aec0befc1d48
alvalle09 <phredvalle@gmail.com>
2021-01-26 20:58:24 -0600

    so easy to add table data in svelte, I like!

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,34 @@
 <script>
-	export let name;
+
+	export let name;	
 	let fetched = false;
 	let count = 0;
+
 	function handleClick() {
 		count += 1;
 		fetched = false;
 	}
-
+	// define `fetch`
 	const fetch = () => {
 		fetched = true;
 		count = 0;
 	}
+
+	// create list array 
+	const list = [
+		{ type: 'Apple', color: 'Green'},
+		{ type: 'Banana', color: 'Yellow'},
+		{ type: 'Grape', color: 'Purple'},
+	]
+
 </script>
 
 <main>
+	<!-- string interpolation of variables -->
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<!-- assign even handler -->
 	<button on:click={handleClick}>
 		Clicked {count} {count === 1 ? 'time' : 'times'}
 	</button>
@@ -28,6 +41,25 @@
 	{#if fetched}
 		<div>done!</div>
 	{/if}
+	
+	<table>
+		<thead>
+			<th>Index</th>
+			<th>Type</th>
+			<th>color</th>
+		</thead>
+		<tbody>
+			<!-- loop through `list` -->
+			{#each list as row, i}
+			<tr>
+				<td>{i}</td>
+				<td>{row.type}</td>
+				<td>{row.color}</td>
+			</tr>
+			{/each}
+		</tbody>
+	</table>
+	
 	
 </main>
 
